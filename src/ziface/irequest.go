e05428dfd0c9dc8629388c3d2881f57989169d9d
xduo <1257180096@qq.com>
2026-03-01 01:02:32 +0800

    feat:request请求消息的封装定义

---
+++ src/ziface/irequest.go
@@ -0,0 +1,12 @@
+package ziface
+
+/*
+IRequest接口：
+实际上是把客户端请求的连接信息，和请求的数据包装到了Request中
+*/
+type IRequest interface {
+	// GetConnection 得到当前连接
+	GetConnection() IConnection
+	// GetData 得到请求的消息数据
+	GetData() []byte
+}
