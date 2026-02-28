d42153ed2bd2e31d27e0a61ef6c6a922bf819df6
xduo <1257180096@qq.com>
2026-03-01 00:23:11 +0800

    feat:连接模块的方法实现

--- src/znet/connect.go
+++ src/znet/connect.go
@@ -1,6 +1,7 @@
 package znet
 
 import (
+	"fmt"
 	"net"
 	"zinx-xduo-study/src/ziface"
 )
@@ -33,4 +34,70 @@ func NewConnection(conn *net.TCPConn, connID uint32, callBackAPI ziface.HandleFu
 		EXitChan:   make(chan bool, 1),
 	}
 	return c
+}
+
+// StratReader 连接的读数据业务方法
+func (c *Connection) StratReader() {
+	fmt.Println("Reader Goroutine is running...")
+	defer fmt.Println("connID = ", c.ConnID, "Reader is exit,remove addr is ", c.RemoteAddr().String())
+	defer c.Stop()
+	for {
+		//读取客户端的数据到buf中，最大512字节
+		buf := make([]byte, 512)
+		cnt, err := c.Conn.Read(buf)
+		if err != nil {
+			fmt.Println("receive buf err", err)
+			continue
+		}
+		//调用当前连接所绑定的HandleAPI
+		err = c.handlerAPI(c.Conn, buf, cnt)
+		if err != nil {
+			fmt.Println("ConnID", c.ConnID, "handle is error", err)
+			break
+		}
+
+	}
+}
+
+// Start 启动连接 让当前的连接准备开始工作
+func (c *Connection) Start() {
+	fmt.Println("Conn Start()... ConnID = ", c.ConnID)
+	//启动从当前连接的读数据业务
+	go c.StratReader()
+	//TODO 启动从当前连接的写数据的业务
+}
+
+// Stop 停止连接 结束当前连接的工作
+func (c *Connection) Stop() {
+	fmt.Println("Conn Stop()... ConnID = ", c.ConnID)
+
+	//如果当前连接已经关闭
+	if c.isClosed == true {
+		return
+	}
+	c.isClosed = true
+	//关闭socket连接
+	c.Conn.Close()
+	//回收资源
+	close(c.EXitChan)
+}
+
+// GetTCPConnection 获取当前连接绑定的socket connection
+func (c *Connection) GetTCPConnection() *net.TCPConn {
+	return c.Conn
+}
+
+// GetConnID 获取当前连接模块的连接ID
+func (c *Connection) GetConnID() uint32 {
+	return c.ConnID
+}
+
+// RemoteAddr 获取远程客户端的 TCP状态 IP port
+func (c *Connection) RemoteAddr() net.Addr {
+	return c.Conn.RemoteAddr()
+}
+
+// Send 发送数据，将数据发送给远程的客户端
+func (c *Connection) Send(data []byte) error {
+	return nil
 }
