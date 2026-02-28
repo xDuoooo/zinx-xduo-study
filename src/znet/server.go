42ed191944fa625cc1cf4a773a3a81cae6276d16
xduo <1257180096@qq.com>
2026-02-28 21:20:17 +0800

    init

--- src/znet/server.go
+++ src/znet/server.go
@@ -1,14 +1,17 @@
 package znet
 
+import "zinx-xduo-study/src/ziface"
+
 // Server IServer 的接口实现，定义一个Server的服务器模块
 type Server struct {
 	//服务器名称
-
+	Name string
 	//服务器绑定的IP版本
-
+	IPVersion string
 	//服务器监听的IP
-
+	IP string
 	//服务器监听的端口
+	Port int
 }
 
 func (s *Server) Start() {
@@ -19,4 +22,17 @@ func (s *Server) Stop() {
 }
 func (s *Server) Server() {
 
+}
+
+/*
+初始化Server模块的方法
+*/
+func NewServer(name string) ziface.IServer {
+	s := &Server{
+		Name:      name,
+		IPVersion: "icp4",
+		IP:        "0.0.0.0",
+		Port:      8999,
+	}
+	return s
 }
