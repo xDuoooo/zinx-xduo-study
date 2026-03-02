7f26568a575392fe74f27d59d083e24e0c306794
xduo <1257180096@qq.com>
2026-03-02 23:02:18 +0800

    feat:V0.7客户端服务端开发

---
+++ src/myDemo/ZinxV0.7/Client/Client0.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net"
+	"time"
+	"zinx-xduo-study/src/znet"
+)
+
+/*
+模拟客户端
+*/
+func main() {
+	fmt.Println("client start...")
+	time.Sleep(1 * time.Second)
+
+	//1. 直接连接远程服务器，得到一个connect连接
+
+	conn, err := net.Dial("tcp", "127.0.0.1:8999")
+	if err != nil {
+		fmt.Println("client start err,exit!")
+
+		return
+	}
+
+	for {
+		//封包消息
+
+		dp := znet.NewDataPack()
+		pack, err := dp.Pack(znet.NewMessage(0, []byte("ZinxV0.6 client Test Message")))
+		if err != nil {
+			fmt.Println("Pack error:", err)
+			break
+		}
+		_, err = conn.Write(pack)
+		if err != nil {
+			fmt.Println("read buf error")
+			break
+		}
+		//读取服务器回复的message
+		head := make([]byte, dp.GetHeadLen())
+		if _, err := io.ReadFull(conn, head); err != nil {
+			fmt.Println("read head error: ", err)
+			break
+		}
+		messageHead, err := dp.UnPack(head)
+		if err != nil {
+			fmt.Println("client unpack message error: ", err)
+			break
+		}
+		if messageHead.GetMsgLen() > 0 {
+			msg := messageHead.(*znet.Message)
+			msg.Data = make([]byte, messageHead.GetMsgLen())
+			_, err := io.ReadFull(conn, msg.Data)
+			if err != nil {
+				fmt.Println("client read message error: ", err)
+				break
+			}
+			fmt.Println(msg.Id, msg.DataLen, string(msg.Data))
+
+		}
+
+		time.Sleep(1 * time.Second)
+	}
+
+}
---
+++ src/myDemo/ZinxV0.7/Server/Server.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	"fmt"
+	"zinx-xduo-study/src/ziface"
+	"zinx-xduo-study/src/znet"
+)
+
+/*
+基于Zinx框架来开发的 服务端应用程序
+*/
+
+type PingRouter struct {
+	znet.BaseRouter
+}
+
+// PreHandle Test PreHandle
+//func (br *PingRouter) PreHandle(request ziface.IRequest) {
+//	fmt.Println("Call Router PreHandle...")
+//	_, err := request.GetConnection().GetTCPConnection().Write([]byte("before ping..."))
+//	if err != nil {
+//		fmt.Println("call back before ping error")
+//	}
+//}
+
+// Handle Test Handle
+func (br *PingRouter) Handle(request ziface.IRequest) {
+	fmt.Println("Call PingRouter Handle...")
+	//先读取客户端的数据，再回写ping..ping..ping
+	fmt.Println("receive from client: msgID = ", request.GetMsgID())
+	fmt.Println("receive from client: message = ", string(request.GetData()))
+
+	err := request.GetConnection().SendMessage(101, []byte("PingRouter..PingRouter..PingRouter"))
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
+// PostHandle Test PostHandle
+//
+//	func (br *PingRouter) PostHandle(request ziface.IRequest) {
+//		fmt.Println("Call Router PostHandle...")
+//		_, err := request.GetConnection().GetTCPConnection().Write([]byte("Post ping..."))
+//		if err != nil {
+//			fmt.Println("call back Post ping error")
+//		}
+//	}
+func main() {
+	//1. 创建一个server句柄 使用 Zinx的Api
+	s := znet.NewServer("[Zinx V0.6]")
+	//2. 自定义路由
+	pingRouter := &PingRouter{}
+	s.AddRouter(0, pingRouter)
+	helloRouter := &HelloZinxRouter{}
+	s.AddRouter(1, helloRouter)
+	//3. 启动Server
+	s.Server()
+}
+
+type HelloZinxRouter struct {
+	znet.BaseRouter
+}
+
+// PreHandle Test PreHandle
+//func (br *PingRouter) PreHandle(request ziface.IRequest) {
+//	fmt.Println("Call Router PreHandle...")
+//	_, err := request.GetConnection().GetTCPConnection().Write([]byte("before ping..."))
+//	if err != nil {
+//		fmt.Println("call back before ping error")
+//	}
+//}
+
+// Handle Test Handle
+func (br *HelloZinxRouter) Handle(request ziface.IRequest) {
+	fmt.Println("Call HelloZinxRouter Handle...")
+	//先读取客户端的数据，再回写ping..ping..ping
+	fmt.Println("receive from client: msgID = ", request.GetMsgID())
+	fmt.Println("receive from client: message = ", string(request.GetData()))
+
+	err := request.GetConnection().SendMessage(201, []byte("helloRouter..helloRouter..helloRouter"))
+	if err != nil {
+		fmt.Println(err)
+	}
+}
